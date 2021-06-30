aeca2caff0dbb4f5976019e6bb2742637ad5a3ad
Michelle Watts <watts.e.michelle@gmail.com>
2021-06-30 10:05:25 +1000

    upadted commits

--- models/Entry.js
+++ models/Entry.js
@@ -1,63 +1,64 @@
-const { Model, DataTypes, Sequelize } = require('sequelize');
+const {
+  Model,
+  DataTypes,
+  Sequelize
+} = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Entry extends Model {}
 
-Entry.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    industry: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    salary: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    start: {
-      type: DataTypes.DECIMAL(4, 2),
-      allowNull: false,
-    },
-    end: {
-      type: DataTypes.DECIMAL(4, 2),
-      allowNull: false,
-    },
-    lunch: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-      defaultValue: false,
-    },
-    unpaidHours: {
-      type: DataTypes.INTEGER,
-    },
-    unpaidSalary: {
-      type: DataTypes.INTEGER,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isEmail: true,
-      },
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.NOW,
+Entry.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  industry: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  salary: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  start: {
+    type: DataTypes.DECIMAL(4, 2),
+    allowNull: true,
+  },
+  end: {
+    type: DataTypes.DECIMAL(4, 2),
+    allowNull: true,
+  },
+  lunch: {
+    type: DataTypes.BOOLEAN,
+    allowNull: true,
+    defaultValue: false,
+  },
+  unpaidHours: {
+    type: DataTypes.INTEGER,
+  },
+  unpaidSalary: {
+    type: DataTypes.INTEGER,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
     },
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'entry',
-  }
-);
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'entry',
+});
 
 module.exports = Entry;
--- public/js/entry.js
+++ public/js/entry.js
@@ -33,9 +33,37 @@ const entryFormHandler = async (unpaidHours, unpaidSalary) => {
   }
 };
 
+
+const entryFormHandlerPart = async (unpaidHours, unpaidSalary) => {
+  const email = document.querySelector('#email-entry-part').value.trim();
+  const industry = document.querySelector('#industry-entry-part').value;
+  const salary = document.querySelector('#salary-gross-part').value.trim();
+
+  if (email && industry && salary) {
+    const response = await fetch('/api/entry', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        industry,
+        salary,
+        unpaidHours,
+        unpaidSalary,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      const responseBody = await response.json();
+      document.location.href = `/results/${responseBody.entryResult.id}`;
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
 const calculate = async () => {
-  const email = document.querySelector('#email-entry').value.trim();
-  const industry = document.querySelector('#industry-entry').value;
   const start = document.querySelector('#start-entry').value;
   const end = document.querySelector('#end-entry').value;
   const salary = document.querySelector('#salary-gross').value.trim();
@@ -43,6 +71,75 @@ const calculate = async () => {
   const lunch = document.querySelector(
     'input[name="lunchOptions"]:checked'
   ).value;
+  event.preventDefault();
+  if (extra === '15') {
+    var extraTimeRes = 0.25 * 365;
+  } else if (extra === '30') {
+    var extraTimeRes = 0.5 * 365;
+  } else if (extra === '45') {
+    var extraTimeRes = 0.75 * 365;
+  } else if (extra === '60') {
+    var extraTimeRes = 1 * 365;
+  } else {
+    var extraTimeRes = 0;
+  }
+  if (end > start) {
+    var oneDay = (end - start) + 24 - 8;
+    if (oneDay < 0) {
+      alert('Please enter full time hours');
+    } else if (lunch === 'yeslunch') {
+      let unpaidHours = (oneDay * 230) + extraTimeRes;
+      let hourlySalary = (salary / 260) / 7.6;
+      let unpaidSalary = hourlySalary * unpaidHours;
+      return entryFormHandler(
+        parseInt(unpaidHours.toFixed(0)),
+        parseInt(unpaidSalary.toFixed(0))
+      );
+    } else {
+      var oneDayNew = oneDay + 0.5;
+      let unpaidHours = (oneDayNew * 230) + extraTimeRes;
+      let hourlySalary = (salary / 260) / 7.6;
+      let unpaidSalary = hourlySalary * unpaidHours;
+      return entryFormHandler(
+        parseInt(unpaidHours.toFixed(0)),
+        parseInt(unpaidSalary.toFixed(0))
+      );
+    }
+  } else {
+    var oneDay = (end - start) - 8;
+    if (oneDay < 0) {
+      alert('Please enter full time hours');
+    } else if (lunch === 'yeslunch') {
+      let unpaidHours = (oneDay * 230) + extraTimeRes;
+      let hourlySalary = (salary / 260) / 7.6;
+      let unpaidSalary = hourlySalary * unpaidHours;
+      return entryFormHandler(
+        parseInt(unpaidHours.toFixed(0)),
+        parseInt(unpaidSalary.toFixed(0))
+      );
+
+    } else {
+      var oneDayNew = oneDay + 0.5;
+      let unpaidHours = (oneDayNew * 230) + extraTimeRes;
+      let hourlySalary = (salary / 260) / 7.6;
+      let unpaidSalary = hourlySalary * unpaidHours;
+      return entryFormHandler(
+        parseInt(unpaidHours.toFixed(0)),
+        parseInt(unpaidSalary.toFixed(0))
+      );
+
+    }
+  }
+}
+
+
+
+const calculatePart = async () => {
+  const paid = document.querySelector('#paid-hours').value;
+  const actual = document.querySelector('#actual-hours').value;
+  const salary = document.querySelector('#salary-gross-part').value.trim();
+  const extra = document.querySelector('input[name="extraTime"]:checked').value;
+
   event.preventDefault();
 
   if (extra === '15') {
@@ -57,113 +154,15 @@ const calculate = async () => {
     var extraTimeRes = 0;
   }
 
-  if (end > start) {
-    var oneDay = (end - start) + 24 - 8;
-    if (oneDay < 0) {
-      alert('Please enter full time hours');
-    } else if (lunch === 'yeslunch') {
-      let unpaidHours = (oneDay * 230) + extraTimeRes;
-      let hourlySalary = (salary / 260) / 7.6;
-      let unpaidSalary = hourlySalary * unpaidHours;
-      // return entryFormHandler(
-      //   parseInt(unpaidHours.toFixed(0)),
-      //   parseInt(unpaidSalary.toFixed(0))
-      // );
-      console.log(
-        unpaidHours.toFixed(0),
-        hourlySalary.toFixed(0),
-        unpaidSalary.toFixed(0),
-        email,
-        "Extra:",
-        extra,
-        "ExtratimeRes:",
-        extraTimeRes,
-        lunch,
-        salary,
-        industry,
-        start,
-        end, hourlySalary, unpaidSalary
-      );
-    } else {
-      var oneDayNew = oneDay + 0.5;
-      let unpaidHours = (oneDayNew * 230) + extraTimeRes;
-      let hourlySalary = (salary / 260) / 7.6;
-      let unpaidSalary = hourlySalary * unpaidHours;
-      // return entryFormHandler(
-      //   parseInt(unpaidHours.toFixed(0)),
-      //   parseInt(unpaidSalary.toFixed(0))
-      // );
-      console.log(
-        unpaidHours.toFixed(0),
-        hourlySalary.toFixed(0),
-        unpaidSalary.toFixed(0),
-        email,
-        "Extra:",
-        extra,
-        "ExtratimeRes",
-        extraTimeRes,
-        lunch,
-        salary,
-        industry,
-        start,
-        end
-      );
-    }
-  } else {
-    var oneDay = (end - start) - 8;
 
-    if (oneDay < 0) {
-      alert('Please enter full time hours');
-    } else if (lunch === 'yeslunch') {
-      let unpaidHours = (oneDay * 230) + extraTimeRes;
-      let hourlySalary = (salary / 260) / 7.6;
-      let unpaidSalary = hourlySalary * unpaidHours;
-      // return entryFormHandler(
-      //   parseInt(unpaidHours.toFixed(0)),
-      //   parseInt(unpaidSalary.toFixed(0))
-      // );
-      console.log(
-        unpaidHours.toFixed(0),
-        hourlySalary.toFixed(0),
-        unpaidSalary.toFixed(0),
-        email,
-        "Extra:",
-        extra,
-        "ExtratimeRes",
-        extraTimeRes,
-        lunch,
-        salary,
-        industry,
-        start,
-        end
-      );
-    } else {
-      var oneDayNew = oneDay + 0.5;
-      let unpaidHours = (oneDayNew * 230) + extraTimeRes;
-      let hourlySalary = (salary / 260) / 7.6;
-      let unpaidSalary = hourlySalary * unpaidHours;
-      // return entryFormHandler(
-      //   parseInt(unpaidHours.toFixed(0)),
-      //   parseInt(unpaidSalary.toFixed(0))
-      // );
-      console.log(
-        unpaidHours.toFixed(0),
-        hourlySalary.toFixed(0),
-        unpaidSalary.toFixed(0),
-        email,
-        "Extra:",
-        extra,
-        "ExtratimeRes",
-        extraTimeRes,
-        lunch,
-        salary,
-        industry,
-        start,
-        end
-      );
-    }
 
-  }
+  let unpaidHours = ((actual - paid) * 46) + extraTimeRes
+  let hourlySalary = (salary / 52) / paid;
+  let unpaidSalary = hourlySalary * unpaidHours;
+  return entryFormHandlerPart(
+    parseInt(unpaidHours.toFixed(0)),
+    parseInt(unpaidSalary.toFixed(0))
+  );
 
 
 }
@@ -172,5 +171,24 @@ const calculate = async () => {
 
 
 
+
+
+
+// console.log(
+//   unpaidHours.toFixed(0),
+//   hourlySalary.toFixed(0),
+//   unpaidSalary.toFixed(0),
+//   email,
+//   "Extra:",
+//   extra,
+//   "ExtratimeRes",
+//   extraTimeRes,
+//   lunch,
+//   salary,
+//   industry,
+//   start,
+//   end
+// );
+
 document.querySelector('#entry-form-full').addEventListener('submit', calculate);
-document.querySelector('#entry-form-part').addEventListener('submit', calculate);
+document.querySelector('#entry-form-part').addEventListener('submit', calculatePart);
