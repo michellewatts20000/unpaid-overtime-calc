deef503902e8803b3314fdbe513d5eda25b84496
Michelle Watts <watts.e.michelle@gmail.com>
2021-07-13 13:20:04 +1000

    manifest file

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+// install event handler
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('static').then(cache => {
+      return cache.addAll([
+        '/',
+        'css/styles.css',
+        'js/entry.js',
+        'js/login.js',
+        'js/logout.js',
+        '/icons/icon-192x192.png',
+        '/icons/icon-256x256.png',
+        '/icons/icon-384x384.png',
+        '/icons/icon-512x512.png',
+        '/manifest.webmanifest',
+      ]);
+    })
+  );
+  console.log('Install');
+  self.skipWaiting();
+});
+
+// retrieve assets from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
